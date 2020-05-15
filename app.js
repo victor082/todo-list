a9c760bb068a67047dff2d448e5ecfbf61dff2b3
Victor Nguyen <victornguyen082@gmail.com>
2020-05-15 00:03:16 -0400

    implement appending new todo onto list

---
+++ app.js
@@ -0,0 +1,40 @@
+// Selectors
+
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault()
+
+    // Todo Div
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+
+    // Create Li
+    const newTodo = document.createElement('li')
+    newTodo.innerText = 'hey'
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo)
+
+    // Check Mark button
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fas fa-check"> </i>'
+    completedButton.classList.add('completed-btn')
+    todoDiv.appendChild(completedButton)
+
+    // Delete button
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"> </i>'
+    trashButton.classList.add('completed-btn')
+    todoDiv.appendChild(trashButton)
+
+    // Append to the list
+    todoList.appendChild(todoDiv)
+}
